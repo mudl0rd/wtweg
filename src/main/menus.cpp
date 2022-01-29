2d4a3154ae5382893fa245edf6fd1695913cef70
mudlord <mudlord@protonmail.com>
2022-01-29 11:16:13 +1100

    Start working on menus.

--- src/main/menus.cpp
+++ src/main/menus.cpp
@@ -3,6 +3,7 @@
 #include "ImGuiFileDialog.h"
 #include "ImGuiFileDialogConfig.h"
 #include "clibretro.h"
+#include <sstream>
 
 ImGuiFileDialog romloader;
 const char* ss_filters = "Savestates (*.state){.state}";
@@ -12,7 +13,7 @@ static ImGuiFileDialogFlags flags = ImGuiFileDialogFlags_Default;
 enum {digi_pad,ana_pad,mousie};
 int controller_type = digi_pad;
 
-
+static bool coresettings = false;
 
 void sdlggerat_menu()
 {
@@ -38,7 +39,8 @@ void sdlggerat_menu()
         if (ImGui::BeginMenu("Options"))
         {
              if (ImGui::MenuItem("Core Settings..."))
-                {}
+                  coresettings = true;
+
             ImGui::Separator();
             if (ImGui::BeginMenu("Input device"))
             {
@@ -97,4 +99,66 @@ void sdlggerat_menu()
     // close
      romloader.Close();
   }
+
+
+
+  if (coresettings) {
+    ImGui::PushItemWidth(200);
+     ImGui::SetNextWindowSize(ImVec2(600, 400), ImGuiCond_FirstUseEver);
+    if (ImGui::Begin("Core Settings"))
+    {
+        
+        for (int i=0;i<instance->core_variables.size();i++)
+        {
+          if(instance->core_variables[i].config_visible)
+          {
+          std::string descript = instance->core_variables[i].description.c_str();
+          std::string var =instance->core_variables[i].var.c_str();
+         
+          std::vector <std::string> options;
+            options.clear();
+            char *pch = (char*)instance->core_variables[i].usevars.c_str();
+			std::stringstream test(pch);
+			std::string segment;
+			std::vector<std::string> seglist;
+			while (std::getline(test, segment, '|'))
+				options.push_back(segment);
+
+        std::string current_item = (char*)options[0].c_str();
+
+ImGui::PushItemWidth(200);
+if (ImGui::BeginCombo(descript.c_str(), current_item.c_str())) // The second parameter is the label previewed before opening the combo.
+{
+    for (int n = 0; n < options.size(); n++)
+    {
+        bool is_selected = (current_item == options[n].c_str()); // You can store your selection however you want, outside or inside your objects
+        if (ImGui::Selectable(options[n].c_str(), is_selected))
+            current_item = options[n].c_str();
+        if (is_selected)
+            ImGui::SetItemDefaultFocus();   // You may set the initial focus when opening the combo (scrolling + for keyboard navigation support)
+    }
+    ImGui::EndCombo();
+}
+
+
+
+
+          }
+        
+        }
+    
+
+        
+
+
+      
+        
+        //click ok when finished adjusting
+        if (ImGui::Button("OK finished adjusting")) {
+            coresettings = false;
+        }
+
+        ImGui::End();
+    }
+}
 }
