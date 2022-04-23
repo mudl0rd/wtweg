2d840434d6922eaa09f362febe28070dd8e5f338
mudlord <mudlord@protonmail.com>
2022-04-23 12:51:42 +1000

    Add base64enc/dec

--- src/main/utils.cpp
+++ src/main/utils.cpp
@@ -22,6 +22,41 @@ static std::string_view SHLIB_EXTENSION = ".dll";
 static std::string_view SHLIB_EXTENSION = ".so";
 #endif
 
+const char *b64tb = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+std::string base64_encode(const std::string &in) {
+    std::string out;
+    int val = 0, valb = -6;
+    for (unsigned char c : in) {
+        val = (val << 8) + c;
+        valb += 8;
+        while (valb >= 0) {
+            out.push_back(b64tb[(val>>valb)&0x3F]);
+            valb -= 6;
+        }
+    }
+    if (valb>-6) out.push_back(b64tb[((val<<8)>>(valb+8))&0x3F]);
+    while (out.size()%4) out.push_back('=');
+    return out;
+}
+
+std::string base64_decode(const std::string &in) {
+    std::string out;
+    std::vector<int> T(256,-1);
+    for (int i=0; i<64; i++) T[b64tb[i]] = i;
+    int val=0, valb=-8;
+    for (unsigned char c : in) {
+        if (T[c] == -1) break;
+        val = (val << 6) + T[c];
+        valb += 6;
+        if (valb >= 0) {
+            out.push_back(char((val>>valb)&0xFF));
+            valb -= 8;
+        }
+    }
+    return out;
+}
+
 unsigned get_filesize(const char *path)
 {
 	std::ifstream is (path, std::ifstream::binary);
--- src/main/utils.h
+++ src/main/utils.h
@@ -19,6 +19,8 @@ void freelib(void *handle);
 unsigned int crc32(const void* data, unsigned int length);
 void vector_appendbytes(std::vector<uint8_t> &vec ,uint8_t* bytes, size_t len);
 std::vector<uint8_t> load_data(const char* path, unsigned * size);
+std::string base64_decode(const std::string &in);
+std::string base64_encode(const std::string &in);
 bool save_data(unsigned char* data, unsigned size, const char* path);
 inline uint32_t pow2up(uint32_t v);
 
