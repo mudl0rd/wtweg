9a95151c4a3a6dc4f27d9299b0ace4b53a5a158b
mudlord <mudlord@protonmail.com>
2022-04-25 03:13:39 +1000

    add mouse support.

--- src/main/input.cpp
+++ src/main/input.cpp
@@ -1,4 +1,5 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_mouse.h>
 #include "resampler.h"
 #include <thread>
 #include <mutex>
@@ -16,157 +17,157 @@
 
 struct key_map
 {
-   unsigned sym;
-   enum retro_key rk;
+    unsigned sym;
+    enum retro_key rk;
 };
 
 const struct key_map key_map_[] = {
-   { SDLK_BACKSPACE, RETROK_BACKSPACE },
-   { SDLK_TAB, RETROK_TAB },
-   { SDLK_CLEAR, RETROK_CLEAR },
-   { SDLK_RETURN, RETROK_RETURN },
-   { SDLK_PAUSE, RETROK_PAUSE },
-   { SDLK_ESCAPE, RETROK_ESCAPE },
-   { SDLK_SPACE, RETROK_SPACE },
-   { SDLK_EXCLAIM, RETROK_EXCLAIM },
-   { SDLK_QUOTEDBL, RETROK_QUOTEDBL },
-   { SDLK_HASH, RETROK_HASH },
-   { SDLK_DOLLAR, RETROK_DOLLAR },
-   { SDLK_AMPERSAND, RETROK_AMPERSAND },
-   { SDLK_QUOTE, RETROK_QUOTE },
-   { SDLK_LEFTPAREN, RETROK_LEFTPAREN },
-   { SDLK_RIGHTPAREN, RETROK_RIGHTPAREN },
-   { SDLK_ASTERISK, RETROK_ASTERISK },
-   { SDLK_PLUS, RETROK_PLUS },
-   { SDLK_COMMA, RETROK_COMMA },
-   { SDLK_MINUS, RETROK_MINUS },
-   { SDLK_PERIOD, RETROK_PERIOD },
-   { SDLK_SLASH, RETROK_SLASH },
-   { SDLK_0, RETROK_0 },
-   { SDLK_1, RETROK_1 },
-   { SDLK_2, RETROK_2 },
-   { SDLK_3, RETROK_3 },
-   { SDLK_4, RETROK_4 },
-   { SDLK_5, RETROK_5 },
-   { SDLK_6, RETROK_6 },
-   { SDLK_7, RETROK_7 },
-   { SDLK_8, RETROK_8 },
-   { SDLK_9, RETROK_9 },
-   { SDLK_COLON, RETROK_COLON },
-   { SDLK_SEMICOLON, RETROK_SEMICOLON },
-   { SDLK_LESS, RETROK_OEM_102 },
-   { SDLK_EQUALS, RETROK_EQUALS },
-   { SDLK_GREATER, RETROK_GREATER },
-   { SDLK_QUESTION, RETROK_QUESTION },
-   { SDLK_AT, RETROK_AT },
-   { SDLK_LEFTBRACKET, RETROK_LEFTBRACKET },
-   { SDLK_BACKSLASH, RETROK_BACKSLASH },
-   { SDLK_RIGHTBRACKET, RETROK_RIGHTBRACKET },
-   { SDLK_CARET, RETROK_CARET },
-   { SDLK_UNDERSCORE, RETROK_UNDERSCORE },
-   { SDLK_BACKQUOTE, RETROK_BACKQUOTE },
-   { SDLK_a, RETROK_a },
-   { SDLK_b, RETROK_b },
-   { SDLK_c, RETROK_c },
-   { SDLK_d, RETROK_d },
-   { SDLK_e, RETROK_e },
-   { SDLK_f, RETROK_f },
-   { SDLK_g, RETROK_g },
-   { SDLK_h, RETROK_h },
-   { SDLK_i, RETROK_i },
-   { SDLK_j, RETROK_j },
-   { SDLK_k, RETROK_k },
-   { SDLK_l, RETROK_l },
-   { SDLK_m, RETROK_m },
-   { SDLK_n, RETROK_n },
-   { SDLK_o, RETROK_o },
-   { SDLK_p, RETROK_p },
-   { SDLK_q, RETROK_q },
-   { SDLK_r, RETROK_r },
-   { SDLK_s, RETROK_s },
-   { SDLK_t, RETROK_t },
-   { SDLK_u, RETROK_u },
-   { SDLK_v, RETROK_v },
-   { SDLK_w, RETROK_w },
-   { SDLK_x, RETROK_x },
-   { SDLK_y, RETROK_y },
-   { SDLK_z, RETROK_z },
-   { SDLK_DELETE, RETROK_DELETE },
-   { SDLK_KP_0, RETROK_KP0 },
-   { SDLK_KP_1, RETROK_KP1 },
-   { SDLK_KP_2, RETROK_KP2 },
-   { SDLK_KP_3, RETROK_KP3 },
-   { SDLK_KP_4, RETROK_KP4 },
-   { SDLK_KP_5, RETROK_KP5 },
-   { SDLK_KP_6, RETROK_KP6 },
-   { SDLK_KP_7, RETROK_KP7 },
-   { SDLK_KP_8, RETROK_KP8 },
-   { SDLK_KP_9, RETROK_KP9 },
-   { SDLK_KP_PERIOD, RETROK_KP_PERIOD },
-   { SDLK_KP_DIVIDE, RETROK_KP_DIVIDE },
-   { SDLK_KP_MULTIPLY, RETROK_KP_MULTIPLY },
-   { SDLK_KP_MINUS, RETROK_KP_MINUS },
-   { SDLK_KP_PLUS, RETROK_KP_PLUS },
-   { SDLK_KP_ENTER, RETROK_KP_ENTER },
-   { SDLK_KP_EQUALS, RETROK_KP_EQUALS },
-   { SDLK_UP, RETROK_UP },
-   { SDLK_DOWN, RETROK_DOWN },
-   { SDLK_RIGHT, RETROK_RIGHT },
-   { SDLK_LEFT, RETROK_LEFT },
-   { SDLK_INSERT, RETROK_INSERT },
-   { SDLK_HOME, RETROK_HOME },
-   { SDLK_END, RETROK_END },
-   { SDLK_PAGEUP, RETROK_PAGEUP },
-   { SDLK_PAGEDOWN, RETROK_PAGEDOWN },
-   { SDLK_F1, RETROK_F1 },
-   { SDLK_F2, RETROK_F2 },
-   { SDLK_F3, RETROK_F3 },
-   { SDLK_F4, RETROK_F4 },
-   { SDLK_F5, RETROK_F5 },
-   { SDLK_F6, RETROK_F6 },
-   { SDLK_F7, RETROK_F7 },
-   { SDLK_F8, RETROK_F8 },
-   { SDLK_F9, RETROK_F9 },
-   { SDLK_F10, RETROK_F10 },
-   { SDLK_F11, RETROK_F11 },
-   { SDLK_F12, RETROK_F12 },
-   { SDLK_F13, RETROK_F13 },
-   { SDLK_F14, RETROK_F14 },
-   { SDLK_F15, RETROK_F15 },
-   { SDLK_NUMLOCKCLEAR, RETROK_NUMLOCK },
-   { SDLK_CAPSLOCK, RETROK_CAPSLOCK },
-   { SDLK_SCROLLLOCK, RETROK_SCROLLOCK },
-   { SDLK_RSHIFT, RETROK_RSHIFT },
-   { SDLK_LSHIFT, RETROK_LSHIFT },
-   { SDLK_RCTRL, RETROK_RCTRL },
-   { SDLK_LCTRL, RETROK_LCTRL },
-   { SDLK_RALT, RETROK_RALT },
-   { SDLK_LALT, RETROK_LALT },
-   /* { ?, RETROK_RMETA }, */
-   /* { ?, RETROK_LMETA }, */
-   { SDLK_LGUI, RETROK_LSUPER },
-   { SDLK_RGUI, RETROK_RSUPER },
-   { SDLK_MODE, RETROK_MODE },
-   { SDLK_HELP, RETROK_HELP },
-   { SDLK_PRINTSCREEN, RETROK_PRINT },
-   { SDLK_SYSREQ, RETROK_SYSREQ },
-   { SDLK_PAUSE, RETROK_BREAK },
-   { SDLK_MENU, RETROK_MENU },
-   { SDLK_POWER, RETROK_POWER },
+    {SDLK_BACKSPACE, RETROK_BACKSPACE},
+    {SDLK_TAB, RETROK_TAB},
+    {SDLK_CLEAR, RETROK_CLEAR},
+    {SDLK_RETURN, RETROK_RETURN},
+    {SDLK_PAUSE, RETROK_PAUSE},
+    {SDLK_ESCAPE, RETROK_ESCAPE},
+    {SDLK_SPACE, RETROK_SPACE},
+    {SDLK_EXCLAIM, RETROK_EXCLAIM},
+    {SDLK_QUOTEDBL, RETROK_QUOTEDBL},
+    {SDLK_HASH, RETROK_HASH},
+    {SDLK_DOLLAR, RETROK_DOLLAR},
+    {SDLK_AMPERSAND, RETROK_AMPERSAND},
+    {SDLK_QUOTE, RETROK_QUOTE},
+    {SDLK_LEFTPAREN, RETROK_LEFTPAREN},
+    {SDLK_RIGHTPAREN, RETROK_RIGHTPAREN},
+    {SDLK_ASTERISK, RETROK_ASTERISK},
+    {SDLK_PLUS, RETROK_PLUS},
+    {SDLK_COMMA, RETROK_COMMA},
+    {SDLK_MINUS, RETROK_MINUS},
+    {SDLK_PERIOD, RETROK_PERIOD},
+    {SDLK_SLASH, RETROK_SLASH},
+    {SDLK_0, RETROK_0},
+    {SDLK_1, RETROK_1},
+    {SDLK_2, RETROK_2},
+    {SDLK_3, RETROK_3},
+    {SDLK_4, RETROK_4},
+    {SDLK_5, RETROK_5},
+    {SDLK_6, RETROK_6},
+    {SDLK_7, RETROK_7},
+    {SDLK_8, RETROK_8},
+    {SDLK_9, RETROK_9},
+    {SDLK_COLON, RETROK_COLON},
+    {SDLK_SEMICOLON, RETROK_SEMICOLON},
+    {SDLK_LESS, RETROK_OEM_102},
+    {SDLK_EQUALS, RETROK_EQUALS},
+    {SDLK_GREATER, RETROK_GREATER},
+    {SDLK_QUESTION, RETROK_QUESTION},
+    {SDLK_AT, RETROK_AT},
+    {SDLK_LEFTBRACKET, RETROK_LEFTBRACKET},
+    {SDLK_BACKSLASH, RETROK_BACKSLASH},
+    {SDLK_RIGHTBRACKET, RETROK_RIGHTBRACKET},
+    {SDLK_CARET, RETROK_CARET},
+    {SDLK_UNDERSCORE, RETROK_UNDERSCORE},
+    {SDLK_BACKQUOTE, RETROK_BACKQUOTE},
+    {SDLK_a, RETROK_a},
+    {SDLK_b, RETROK_b},
+    {SDLK_c, RETROK_c},
+    {SDLK_d, RETROK_d},
+    {SDLK_e, RETROK_e},
+    {SDLK_f, RETROK_f},
+    {SDLK_g, RETROK_g},
+    {SDLK_h, RETROK_h},
+    {SDLK_i, RETROK_i},
+    {SDLK_j, RETROK_j},
+    {SDLK_k, RETROK_k},
+    {SDLK_l, RETROK_l},
+    {SDLK_m, RETROK_m},
+    {SDLK_n, RETROK_n},
+    {SDLK_o, RETROK_o},
+    {SDLK_p, RETROK_p},
+    {SDLK_q, RETROK_q},
+    {SDLK_r, RETROK_r},
+    {SDLK_s, RETROK_s},
+    {SDLK_t, RETROK_t},
+    {SDLK_u, RETROK_u},
+    {SDLK_v, RETROK_v},
+    {SDLK_w, RETROK_w},
+    {SDLK_x, RETROK_x},
+    {SDLK_y, RETROK_y},
+    {SDLK_z, RETROK_z},
+    {SDLK_DELETE, RETROK_DELETE},
+    {SDLK_KP_0, RETROK_KP0},
+    {SDLK_KP_1, RETROK_KP1},
+    {SDLK_KP_2, RETROK_KP2},
+    {SDLK_KP_3, RETROK_KP3},
+    {SDLK_KP_4, RETROK_KP4},
+    {SDLK_KP_5, RETROK_KP5},
+    {SDLK_KP_6, RETROK_KP6},
+    {SDLK_KP_7, RETROK_KP7},
+    {SDLK_KP_8, RETROK_KP8},
+    {SDLK_KP_9, RETROK_KP9},
+    {SDLK_KP_PERIOD, RETROK_KP_PERIOD},
+    {SDLK_KP_DIVIDE, RETROK_KP_DIVIDE},
+    {SDLK_KP_MULTIPLY, RETROK_KP_MULTIPLY},
+    {SDLK_KP_MINUS, RETROK_KP_MINUS},
+    {SDLK_KP_PLUS, RETROK_KP_PLUS},
+    {SDLK_KP_ENTER, RETROK_KP_ENTER},
+    {SDLK_KP_EQUALS, RETROK_KP_EQUALS},
+    {SDLK_UP, RETROK_UP},
+    {SDLK_DOWN, RETROK_DOWN},
+    {SDLK_RIGHT, RETROK_RIGHT},
+    {SDLK_LEFT, RETROK_LEFT},
+    {SDLK_INSERT, RETROK_INSERT},
+    {SDLK_HOME, RETROK_HOME},
+    {SDLK_END, RETROK_END},
+    {SDLK_PAGEUP, RETROK_PAGEUP},
+    {SDLK_PAGEDOWN, RETROK_PAGEDOWN},
+    {SDLK_F1, RETROK_F1},
+    {SDLK_F2, RETROK_F2},
+    {SDLK_F3, RETROK_F3},
+    {SDLK_F4, RETROK_F4},
+    {SDLK_F5, RETROK_F5},
+    {SDLK_F6, RETROK_F6},
+    {SDLK_F7, RETROK_F7},
+    {SDLK_F8, RETROK_F8},
+    {SDLK_F9, RETROK_F9},
+    {SDLK_F10, RETROK_F10},
+    {SDLK_F11, RETROK_F11},
+    {SDLK_F12, RETROK_F12},
+    {SDLK_F13, RETROK_F13},
+    {SDLK_F14, RETROK_F14},
+    {SDLK_F15, RETROK_F15},
+    {SDLK_NUMLOCKCLEAR, RETROK_NUMLOCK},
+    {SDLK_CAPSLOCK, RETROK_CAPSLOCK},
+    {SDLK_SCROLLLOCK, RETROK_SCROLLOCK},
+    {SDLK_RSHIFT, RETROK_RSHIFT},
+    {SDLK_LSHIFT, RETROK_LSHIFT},
+    {SDLK_RCTRL, RETROK_RCTRL},
+    {SDLK_LCTRL, RETROK_LCTRL},
+    {SDLK_RALT, RETROK_RALT},
+    {SDLK_LALT, RETROK_LALT},
+    /* { ?, RETROK_RMETA }, */
+    /* { ?, RETROK_LMETA }, */
+    {SDLK_LGUI, RETROK_LSUPER},
+    {SDLK_RGUI, RETROK_RSUPER},
+    {SDLK_MODE, RETROK_MODE},
+    {SDLK_HELP, RETROK_HELP},
+    {SDLK_PRINTSCREEN, RETROK_PRINT},
+    {SDLK_SYSREQ, RETROK_SYSREQ},
+    {SDLK_PAUSE, RETROK_BREAK},
+    {SDLK_MENU, RETROK_MENU},
+    {SDLK_POWER, RETROK_POWER},
 
-   { SDLK_UNDO, RETROK_UNDO },
+    {SDLK_UNDO, RETROK_UNDO},
 
-   { 0, RETROK_UNKNOWN },
+    {0, RETROK_UNKNOWN},
 };
 
 static bool key_pressed(int key)
 {
-   int num_keys;
-   const uint8_t *keymap = SDL_GetKeyboardState(&num_keys);
-   unsigned sym          = SDL_GetScancodeFromKey((SDL_Keycode)key_map_[(enum retro_key)key].sym);
-   if (sym >= (unsigned)num_keys)
-      return false;
-   return keymap[sym];
+    int num_keys;
+    const uint8_t *keymap = SDL_GetKeyboardState(&num_keys);
+    unsigned sym = SDL_GetScancodeFromKey((SDL_Keycode)key_map_[(enum retro_key)key].sym);
+    if (sym >= (unsigned)num_keys)
+        return false;
+    return keymap[sym];
 }
 
 SDL_Joystick *Joystick = NULL;
@@ -373,12 +374,11 @@ bool checkbuttons_forui(int selected_inp, bool *isselected_inp)
     int axesCount = SDL_JoystickNumAxes(Joystick);
     auto &bind = lib->core_inputbinds[selected_inp];
 
-
-    int numkeys=0;
-    const Uint8* keyboard = SDL_GetKeyboardState(&numkeys);
-    for(int i=0;i<numkeys;i++)
+    int numkeys = 0;
+    const Uint8 *keyboard = SDL_GetKeyboardState(&numkeys);
+    for (int i = 0; i < numkeys; i++)
     {
-        if(keyboard[i])
+        if (keyboard[i])
         {
             bind.joykey_desc = SDL_GetScancodeName((SDL_Scancode)i);
             bind.config.bits.sdl_id = i;
@@ -389,7 +389,6 @@ bool checkbuttons_forui(int selected_inp, bool *isselected_inp)
             return true;
         }
     }
-
 
     for (int a = 0; a < axesCount; a++)
     {
@@ -552,7 +551,7 @@ void poll_lr()
         else if (bind.config.bits.joytype == joytype_::button)
             bind.val = SDL_JoystickGetButton(Joystick, bind.config.bits.sdl_id);
         else if (bind.config.bits.joytype == joytype_::keyboard)
-            bind.val = SDL_GetKeyboardState(NULL)[bind.config.bits.sdl_id];  
+            bind.val = SDL_GetKeyboardState(NULL)[bind.config.bits.sdl_id];
     }
 }
 
@@ -565,11 +564,32 @@ int16_t input_state(unsigned port, unsigned device, unsigned index,
 
     if (device == RETRO_DEVICE_MOUSE)
     {
+        int x = 0;
+        int y = 0;
+        Uint8 btn = SDL_GetRelativeMouseState(&x, &y);
+        switch (id)
+        {
+        case RETRO_DEVICE_ID_MOUSE_LEFT:
+            return (SDL_BUTTON(SDL_BUTTON_LEFT) & btn);
+        case RETRO_DEVICE_ID_MOUSE_RIGHT:
+            return (SDL_BUTTON(SDL_BUTTON_RIGHT) & btn);
+        case RETRO_DEVICE_ID_MOUSE_X:
+            return x;
+        case RETRO_DEVICE_ID_MOUSE_Y:
+            return y;
+        case RETRO_DEVICE_ID_MOUSE_MIDDLE:
+            return (SDL_BUTTON(SDL_BUTTON_MIDDLE) & btn);
+        case RETRO_DEVICE_ID_MOUSE_BUTTON_4:
+            return (SDL_BUTTON(SDL_BUTTON_X1) & btn);
+        case RETRO_DEVICE_ID_MOUSE_BUTTON_5:
+            return (SDL_BUTTON(SDL_BUTTON_X2) & btn);
+        default:
         return 0;
+        }
     }
 
-    if(device == RETRO_DEVICE_KEYBOARD)
-    return (id < RETROK_LAST) && key_pressed(id);
+    if (device == RETRO_DEVICE_KEYBOARD)
+        return (id < RETROK_LAST) && key_pressed(id);
 
     if (device == RETRO_DEVICE_ANALOG)
     {
