385ae9b63c38090919922191dda459cd22650315
Crudux Cruo <cruduxcru0@proton.me>
2023-11-07 19:13:19 +1100

    add LZMA/DEFLATE compression.

--- src/main/mudutils/compress.cpp
+++ src/main/mudutils/compress.cpp
@@ -15,8 +15,59 @@
 #endif
 using namespace std;
 
+#include "7z_C/LzmaLib.h"
+#include "miniz.h"
+
 namespace MudUtil
 {
+    std::vector<unsigned char> compress_deflate(unsigned char *buf, size_t size)
+    {
+        std::vector<unsigned char> out;
+        mz_ulong out_len = mz_compressBound(size);
+        out.resize(out_len);
+        mz_compress(out.data(), &out_len, buf,size);
+        out.resize(out_len);
+        return out;
+    }
 
+    std::vector<unsigned char> decompress_deflate(
+        uint8_t *buf,
+        size_t size, size_t uncomp_sz)
+    {
+        std::vector<unsigned char> out;
+        out.resize(uncomp_sz);
+        mz_ulong us=uncomp_sz;
+        mz_uncompress(out.data(),&us,buf,size);
+        return out;
+    }
 
+    std::vector<unsigned char> compress_lzma(unsigned char *buf, size_t size)
+    {
+        std::vector<unsigned char> out;
+        size_t props = LZMA_PROPS_SIZE;
+        size_t dst_bound = size + size / 3 + 128;
+        out.resize(props + dst_bound);
+        int res = LzmaCompress(
+            &out[LZMA_PROPS_SIZE], &dst_bound,
+            &buf[0], size,
+            &out[0], &props,
+            -1, 0, -1, -1, -1, -1, -1);
+        out.resize(props + dst_bound);
+        return out;
+    }
+
+    std::vector<unsigned char> decompress_lzma(
+        uint8_t *buf,
+        size_t size, size_t uncomp_sz)
+    {
+        std::vector<unsigned char> out;
+        out.resize(uncomp_sz);
+        size_t src_len = size - LZMA_PROPS_SIZE;
+        SRes res = LzmaUncompress(
+            &out[0], &uncomp_sz,
+            &buf[LZMA_PROPS_SIZE], &src_len,
+            &buf[0], LZMA_PROPS_SIZE);
+        out.resize(uncomp_sz); // If uncompressed data can be smaller
+        return out;
+    }
 }
