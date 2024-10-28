199730c960e24f58d5f4c73b694e127b43e57929
mudl0rd <allegedlywrong@proton.me>
2024-10-28 13:45:52 +1100

    add memory file reading/writing.

--- src/main/mudutils/misc.cpp
+++ src/main/mudutils/misc.cpp
@@ -236,4 +236,123 @@ namespace MudUtil
 		}
 		return out;
 	}
+
+
+MEM *mopen(const int8_t *src, uint32_t length)
+{
+	MEM *b;
+
+	if ((src == NULL) || (length <= 0)) return (NULL);
+
+	b = (MEM *)(malloc(sizeof (MEM)));
+	if (b == NULL) return (NULL);
+
+	b->_base   = (int8_t *)(src);
+	b->_ptr    = (int8_t *)(src);
+	b->_cnt    = length;
+	b->_bufsiz = length;
+	b->_eof    = 0;
+
+	return (b);
+}
+
+void mclose(MEM *buf)
+{
+	if (buf != NULL)
+	{
+		free(buf);
+		buf = NULL;
+	}
+}
+
+int32_t mtell(MEM *buf)
+{
+	return (buf->_bufsiz - buf->_cnt);
+}
+
+size_t mread(void *buffer, size_t size, size_t count, MEM *buf)
+{
+	size_t wrcnt;
+	int32_t pcnt;
+
+	if (buf       == NULL) return (0);
+	if (buf->_ptr == NULL) return (0);
+
+	wrcnt = size * count;
+	if ((size == 0) || buf->_eof) return (0);
+
+	pcnt = ((uint32_t)(buf->_cnt) > wrcnt) ? wrcnt : buf->_cnt;
+	memcpy(buffer, buf->_ptr, pcnt);
+
+	buf->_cnt -= pcnt;
+	buf->_ptr += pcnt;
+
+	if (buf->_cnt <= 0)
+	{
+		buf->_ptr = buf->_base + buf->_bufsiz;
+		buf->_cnt = 0;
+		buf->_eof = 1;
+	}
+
+	return (pcnt / size);
+}
+
+size_t mwrite(const void *buffer, size_t size, size_t count, MEM *buf)
+{
+	size_t wrcnt;
+	int32_t pcnt;
+
+	if (buf       == NULL) return (0);
+	if (buf->_ptr == NULL) return (0);
+
+	wrcnt = size * count;
+	if ((size == 0) || buf->_eof) return (0);
+
+	pcnt = ((uint32_t)(buf->_cnt) > wrcnt) ? wrcnt : buf->_cnt;
+	memcpy(buf->_ptr, buffer, pcnt);
+
+	buf->_cnt -= pcnt;
+	buf->_ptr += pcnt;
+
+	if (buf->_cnt <= 0)
+	{
+		buf->_ptr = buf->_base + buf->_bufsiz;
+		buf->_cnt = 0;
+		buf->_eof = 1;
+	}
+
+	return (pcnt / size);
+}
+
+int32_t meof(MEM *buf)
+{
+	if (buf == NULL) return (1); // XXX: Should return a different value?
+
+	return (buf->_eof);
+}
+
+void mseek(MEM *buf, int32_t offset, int32_t whence)
+{
+	if (buf == NULL) return;
+
+	if (buf->_base)
+	{
+		switch (whence)
+		{
+		case SEEK_SET: buf->_ptr  = buf->_base + offset;                break;
+		case SEEK_CUR: buf->_ptr += offset;                             break;
+		case SEEK_END: buf->_ptr  = buf->_base + buf->_bufsiz + offset; break;
+		default: break;
+		}
+
+		buf->_eof = 0;
+		if (buf->_ptr >= (buf->_base + buf->_bufsiz))
+		{
+			buf->_ptr = buf->_base + buf->_bufsiz;
+			buf->_eof = 1;
+		}
+
+		buf->_cnt = (buf->_base + buf->_bufsiz) - buf->_ptr;
+	}
+}
 }
